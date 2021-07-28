b17b501a890a61dd9d5bf743607a1b264d90419c
Abderahmane Aoufi <abdouou7@gmail.com>
2021-07-28 12:40:08 +0100

    start building the setup fucntion

--- Greeter.js
+++ Greeter.js
@@ -1,3 +1,15 @@
 (function (global, $) {
-   console.log("start the library ...");
+  var Greeter = function (firstName, lastName, language) {
+    return new Greeter.init(firstName, lastName, language); // like creating object of class
+  };
+  // function constructor that build on aobject and gives it three prorities ...
+  Greeter.init = function (firstName, lastName, language) {
+    var self = this;
+    self["firstName"] = firstName || "";
+    self["lastName"] = lastName || "";
+    self["language"] = language || "en";
+  };
+
+  Greeter.init.prototype = Greeter.prototype; // setting the prototype of that fucntion to the mother funciton
+  console.log(Greeter("Aoufi", "Abderahmane", "ar"));
 })(window, jQuery);
